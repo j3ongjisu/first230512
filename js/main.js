c272c687306bd93df7a8a763dbe5ea671af21c38
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-05-17 09:11:40 +0900

    230512

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.topBanner .inner i').on('click', function () {
+        $('.topBanner').addClass('on')
+    });
+
+    $('.mainSlide').slick({
+        arrows: false,
+        dots: true,
+    });
+})
+
